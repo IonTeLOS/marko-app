2a1f33a0b4343fc5e7e2fba37a4d0cbd467c2d29
Ion <teloslinux@protonmail.com>
2024-08-17 15:08:56 +0300

    Delete edge-functions directory

--- edge-functions/hello-world.js
+++
@@ -1,6 +0,0 @@
-// netlify/edge-functions/hello-world.js
-export default () => new Response("Hello WWWorld", {
-  headers: { "Content-Type": "text/plain" },
-});
-
-export const config = { path: "/hi" };
