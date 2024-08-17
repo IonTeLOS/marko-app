5f5e4610d1aaad20b5e94f4664201998088e4f8e
Ion <teloslinux@protonmail.com>
2024-08-17 14:53:18 +0300

    Update hello-world.js

--- edge-functions/hello-world.js
+++ edge-functions/hello-world.js
@@ -1,7 +1,5 @@
-// netlify/edge-functions/hello-world.js
-export default async (request, context) => {
-  return new Response("Hello, WWWorld!", {
-    headers: { "content-type": "text/plain" },
-  });
-};
+export default () => new Response("Hello WWWorld");
+
+export const config = { path: "/hi" };
+
  
