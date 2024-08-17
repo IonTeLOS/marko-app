5345593ed836b1a5a37f6b263ce0fd88ff38eabd
Ion <teloslinux@protonmail.com>
2024-08-17 15:24:39 +0300

    Delete marko-edge-functions directory

--- marko-edge-functions/hello-world.js
+++
@@ -1,7 +0,0 @@
-// netlify/edge-functions/hello-world.js
-export default async (request, context) => {
-  return new Response("Hello, WWWorld!", {
-    headers: { "content-type": "text/plain" },
-  });
-};
- 
