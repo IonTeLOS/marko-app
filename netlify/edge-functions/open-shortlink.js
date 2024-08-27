8301abdfca8a1634f1a6d9b75289076b94c0025a
Ion <teloslinux@protonmail.com>
2024-08-27 14:38:34 +0300

    Update open-shortlink.js

--- netlify/edge-functions/open-shortlink.js
+++ netlify/edge-functions/open-shortlink.js
@@ -131,6 +131,7 @@ export default async (request, context) => {
                   </div>
                   <!-- Materialize JS -->
                   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+                  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
                 </body>
                 </html>
               `;
